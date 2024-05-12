b6ebdfed8fcb24e24a2d1a4eeaf6f09ebed99ded
Giordano Arcieri <arcierigiordano@gmail.com>
2024-05-12 13:33:32 -0400

    Finished fuzzyfication func

--- scripts/main.py
+++ scripts/main.py
@@ -2,14 +2,33 @@ from MFIS_Classes import *
 from MFIS_Read_Functions import *
 
 
-def FuzzyfyApplication(app: Application, input_var_sets: FuzzySetsDict) -> FuzzySet:
-    fuzzified_app = FuzzySet()
-    fuzzified_app.x = 0
-    fuzzified_app.y = 0
+def FuzzyfyApplication(app: Application, input_var_sets: FuzzySetsDict) -> dict:
+    fuzzified_app: dict = dict()
+    for item in app.data:
+        attribute = item[0]
+        value = item[1]
+        fuzzy_values = dict()
+        for key, sets in input_var_sets.items():
+            if (key.split('=')[0] == attribute):
+                # Get the index of the x value in the array x
+                index = np.where(sets.x == value)[0]
+                if len(index) > 0:  # Check if the x value exists in the array x
+                    fuzzy_values.update({key.split('=')[1]: sets.y[index[0]]})
+            else:
+                continue
+
+        fuzzified_app.update({attribute: fuzzy_values})
+
     return fuzzified_app
 
 
 def ApplyRules(fuzzy_set: FuzzySet, rules: RuleList) -> str:
+    # Rule ruleName = rule01, consequent = Risk=HighR, antecedent = [IncomeLevel=Hig, Assets=Abundant, Amount=Small]
+    
+    
+    for rule in rules:
+        print(rule.ruleName, rule.consequent, rule.antecedent)
+    
     return "HighR"
 
 
@@ -17,10 +36,10 @@ def DefuzzyfyRisk(risk: str, output_var_sets: FuzzySetsDict) -> float:
     return 0
 
 
-def plotFuzzySets(fuzzySetsDict):
-    
-    prev:str = ""
-    
+def plotFuzzySets(fuzzySetsDict: FuzzySetsDict):
+
+    prev: str = ""
+
     # var: str, sets: FuzzySet
     for var, sets in fuzzySetsDict.items():
         current_group = var.split('=')[0]
@@ -47,33 +66,6 @@ def plotFuzzySets(fuzzySetsDict):
         plt.legend()
         plt.savefig('Graphs/' + prev + '.png')
         plt.close()
-            
-
-    return 
-    # Generating x and y values for the graph
-    x_values = np.linspace(xmin, xmax, 1000)
-    y_values = np.zeros_like(x_values)
-    y_values[(x_values >= a) & (x_values <= b)] = (
-        x_values[(x_values >= a) & (x_values <= b)] - a) / (b - a)
-    y_values[(x_values > b) & (x_values < c)] = 1
-    y_values[(x_values >= c) & (x_values <= d)] = (
-        d - x_values[(x_values >= c) & (x_values <= d)]) / (d - c)
-
-    # Plot the graph
-    plt.figure(figsize=(10, 5))
-    plt.plot(x_values, y_values, color='red')
-    plt.title(title, pad=20)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.ylim(-0.01, 1.1)  # Set the y-axis limit
-    plt.xlim(xmin, xmax)  # Set the x-axis limit
-    plt.grid(True)
-    plt.xticks(np.arange(xmin, xmax + 1, (xmax - xmin) / 5))
-    plt.yticks(np.arange(0, 1.25, 0.25))
-    plt.legend()
-
-    # Show the plot
-    plt.savefig('Graphs/' + title + '.png')
 
 
 def main():
@@ -82,26 +74,24 @@ def main():
     output_var_sets = readFuzzySetsFile('InputTextFiles/Risks.txt')
     rules = readRulesFile()
     applications: list[Application] = readApplicationsFile()
-    
+
+    # Plot all fuzzy graphs
     plotFuzzySets(input_var_sets)
     plotFuzzySets(output_var_sets)
-        
-    
-    
-    return 
 
     with open('Results.txt', 'w') as file:
         for app in applications:
             # Print application
             app.printApplication()
             # Fuzzyfy application
-            fuzzy_set: FuzzySet = FuzzyfyApplication(app, input_var_sets)
+            fuzzy_nums: dict = FuzzyfyApplication(app, input_var_sets)
             # Apply rules
-            risk: str = ApplyRules(fuzzy_set, rules)
-            # Defuzzyfy application
-            risk_value: float = DefuzzyfyRisk(risk, output_var_sets)
-            # Write results to file
-            file.write(f"App ID: {app.appId}, Risk Level: {risk_value}\n")
+            risk: str = ApplyRules(fuzzy_nums, rules)
+            return
+            # # Defuzzyfy application
+            # risk_value: float = DefuzzyfyRisk(risk, output_var_sets)
+            # # Write results to file
+            # file.write(f"App ID: {app.appId}, Risk Level: {risk_value}\n")
 
 
 if __name__ == "__main__":
